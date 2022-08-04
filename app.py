a2487a72719732b9c1a4e21aae38d39a50be227e
Shivansh Srivastava <srivastavashiv0112@gmail.com>
2022-08-04 16:42:21 +0530

    make

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS,cross_origin
 import numpy as np
 from pickle import load
+import os
 
 app=Flask(__name__)
 
@@ -54,4 +55,4 @@ def index():
             return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True,port=5000)
+    app.run(debug=True,port=int(os.environ.get('PORT', 5000)))
